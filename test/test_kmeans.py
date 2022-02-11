5677add8fa67f906c2f97fdfbe5f9578f80b18db
Jovia Nierenberg <jovia.nierenberg@ucsf.edu>
2022-02-10 16:05:29 -0800

    testing dependencies

--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -1,1 +1,7 @@
+# Importing Dependencies
+import pytest
+import numpy as np
+from scipy.spatial.distance import cdist
+from cluster import KMeans
+
 # Write your k-means unit tests here
--- test/test_silhouette.py
+++ test/test_silhouette.py
@@ -1,1 +1,7 @@
+# Importing Dependencies
+import pytest
+import numpy as np
+from scipy.spatial.distance import cdist
+from cluster import Silhouette
+
 # write your silhouette score unit tests here
