423fcf817e5bfd6eb0264e713e4772ff9855405c
Jovia Nierenberg <jovia.nierenberg@ucsf.edu>
2022-02-10 20:54:37 -0800

    silhouette scores

--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -8,6 +8,7 @@ class Silhouette:
             metric: str
                 the name of the distance metric to use
         """
+        self.metric = metric
 
     def score(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
         """
@@ -24,5 +25,28 @@ class Silhouette:
             np.ndarray
                 a 1D array with the silhouette scores for each of the observations in `X`
         """
-
-
+        # calculate distances between all points
+        dists = cdist(X, X, metric=self.metric)
+        
+        # count observations
+        num_obs = np.shape(y)[0]
+        
+        # create empty 1d array for scores
+        scores = np.empty(shape=(num_obs,1))
+        
+        # calculate score for each observation
+        for obs in range(num_obs): 
+            
+            # determine cluster membership and calculate mean in cluster distance from obs
+            curr_cluster = y[obs]
+            in_cluster = np.where(y==curr_cluster)[0] # indices of observations in the cluster
+            out_of_cluster = np.where(y!=curr_cluster)[0] # indices of observations outside the cluster 
+            a = np.mean([dists[obs, other] for other in in_cluster if not obs==other])
+            
+            # calculate mean nearest cluster distance from obs
+            b = np.min([np.mean([dists[obs, other] for other in out_of_cluster]) for cluster in set(y) if not cluster==curr_cluster]) ## need to make nearest cluster
+            
+            # calculate silhouette distance
+            scores[obs] = (b-a)/max(b,a)
+        
+        return scores
